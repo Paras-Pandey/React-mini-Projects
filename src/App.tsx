325ed7a8b55f04fe1df35d6bbdcfee80d939e7f5
Paras Pandey <88756589+Paras-Pandey@users.noreply.github.com>
2023-10-18 11:27:27 +0000

    Add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import "./App.css";
 import Todo from "./To-do/toDo";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Todo />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/todo">To-do</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/" element={<h1>Home Page</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
